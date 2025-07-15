37c432ed0891ce9bdb0b3e6ce893660a7314c4ae
Nikhil <nikhil150itc23@bpitindia.edu.in>
2025-07-16 02:59:53 +0530

    Time: 30 ms (93.34%) | Memory: 18.4 MB (81.86%) - LeetSync

---
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         ListNode* temp2 = headB;
+//        while(temp2 != NULL){
+//         ListNode* temp1 = headA;
+//         while(temp1 != NULL){
+//             if(temp2 == temp1){
+//                 return temp1;
+//             }
+//             else{
+//                 temp1=temp1->next;
+//             }
+
+//         }
+//         temp2=temp2->next;
+//        }
+//        return NULL; 
+//     }
+// };
+class Solution {
+public:
+    long getLen(ListNode* head){
+        ListNode* temp = head;
+        long len = 1;
+        while(temp->next != NULL){
+            temp=temp->next;
+            len++;
+        }
+        return len;
+    }
+    ListNode* getIntersectionNode(ListNode *headA, ListNode *headB) {
+        long lenA = getLen(headA);
+        long lenB = getLen(headB);
+        
+        // Advance the pointer of the longer list
+        while(lenA > lenB) {
+            headA = headA->next;
+            lenA--;
+        }
+        while(lenB > lenA) {
+            headB = headB->next;
+            lenB--;
+        }
+
+        // Move both pointers together
+        while(headA != nullptr && headB != nullptr) {
+            if(headA == headB) {
+                return headA;
+            }
+            headA = headA->next;
+            headB = headB->next;
+        }
+
+        return nullptr;
+    }
+};
