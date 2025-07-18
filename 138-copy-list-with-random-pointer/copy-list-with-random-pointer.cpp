0c3caf1cf3b8f05db748cba8105398b0cd46cddd
Nikhil <nikhil150itc23@bpitindia.edu.in>
2025-07-19 01:25:02 +0530

    Time: 7 ms (52.49%) | Memory: 15.1 MB (68.82%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -13,29 +13,82 @@ public:
     }
 };
 */
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
+//         if(head == NULL){
+//             return NULL;
+//         }
+//         unordered_map<Node*,Node*>mpp;
+//         Node* temp = head;
+//         while(temp != NULL){
+//             mpp[temp]= new Node(temp->val);
+//             temp=temp->next;
+
+//         }
+//         temp = head;
+//         while(temp != NULL){
+//             mpp[temp]->next = mpp[temp->next];
+//             if(temp->random != NULL){
+//                 mpp[temp]->random = mpp[temp->random];
+//             }
+//             temp= temp->next;
+//         }
+//         temp = head;
+//         return mpp[temp];
+//     }
+// };
 
 class Solution {
 public:
+    void addCopy(Node* &head){
+        Node* temp1 = head;
+        if(temp1 == NULL){
+            return;
+        }
+        // ek case khud solve kardo
+        Node* temp2 = temp1->next;
+        temp1->next = new Node(temp1->val);
+        temp1= temp1->next;
+        temp1->next = temp2;
+        addCopy(temp2);
+    }
+    void seperationOfCopy(Node* temp1, Node* temp2){
+        if(temp1 == NULL){
+            return;
+        }
+        //ek case khud solve kardo
+        temp1->next = temp2->next;
+        temp1 = temp1->next;
+        if(temp1 != NULL){
+        temp2->next = temp1->next;
+        temp2 = temp2->next;
+        }
+        seperationOfCopy(temp1,temp2);
+
+    }
     Node* copyRandomList(Node* head) {
         if(head == NULL){
             return NULL;
         }
-        unordered_map<Node*,Node*>mpp;
+        // this function will add copy nodes in next to each original node.
+        addCopy(head);
+
+        // copying the random nodes of original lists to the copyed list corresponding nodes 
+        // As original node ->next => copyed node
         Node* temp = head;
         while(temp != NULL){
-            mpp[temp]= new Node(temp->val);
-            temp=temp->next;
-
-        }
-        temp = head;
-        while(temp != NULL){
-            mpp[temp]->next = mpp[temp->next];
             if(temp->random != NULL){
-                mpp[temp]->random = mpp[temp->random];
+            temp->next->random = temp->random->next;
             }
             temp= temp->next;
+            temp= temp->next;
         }
-        temp = head;
-        return mpp[temp];
+       // recursive call to break the link of Original and Copyed nodes to seperate them
+       Node* copyHead = head->next;
+       seperationOfCopy(head,copyHead);
+       return copyHead; 
+
+
     }
 };
