168650a3f6ff5a9f019854fbb29e8cb0899f94a6
Nikhil <nikhil150itc23@bpitindia.edu.in>
2025-07-25 21:51:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (26.75%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,59 +1,109 @@
+// class MyQueue {
+// public:
+//     stack<int>st;
+//     MyQueue() {
+        
+//     }
+    
+//     void push(int x) {
+//         st.push(x);
+//     }
+
+//     int frontEle = -1;
+//     int findFrontEle(){
+//           //Base case
+//         if(st.size() == 1){
+//             frontEle = st.top();
+//             return frontEle;
+//         }
+//         //ek case khud solve kardo
+//         int temp =st.top();
+//         st.pop();
+//         findFrontEle();
+//         st.push(temp);
+//         return frontEle;  
+//     }
+
+//     int popedEle = -1;
+//     int pop() {
+//         //Base case
+//         if(st.size() == 2){
+//             frontEle = st.top();
+//         }
+//         if(st.size() == 1){
+//             popedEle = st.top();
+//             st.pop();
+//             return popedEle;
+//         }
+//         //ek case khud solve kardo
+//         int temp =st.top();
+//         st.pop();
+//         pop();
+//         st.push(temp);
+//         return popedEle;
+//     }
+    
+//     int peek() {
+//         if(frontEle == -1){
+//           return findFrontEle();
+//         }
+//         else{
+//         return frontEle;
+//         }
+//     }
+    
+//     bool empty() {
+//         return st.empty();
+//     }
+// };
+
 class MyQueue {
 public:
-    stack<int>st;
+    stack<int>s1;
+    stack<int>s2;
     MyQueue() {
         
     }
     
     void push(int x) {
-        st.push(x);
+       s1.push(x); 
     }
-
-    int frontEle = -1;
-    int findFrontEle(){
-          //Base case
-        if(st.size() == 1){
-            frontEle = st.top();
-            return frontEle;
-        }
-        //ek case khud solve kardo
-        int temp =st.top();
-        st.pop();
-        findFrontEle();
-        st.push(temp);
-        return frontEle;  
-    }
-
-    int popedEle = -1;
+    
     int pop() {
-        //Base case
-        if(st.size() == 2){
-            frontEle = st.top();
+        if(!s2.empty()){
+            int topele = s2.top();
+            s2.pop();
+            return topele;
         }
-        if(st.size() == 1){
-            popedEle = st.top();
-            st.pop();
-            return popedEle;
+        else{
+            while(!s1.empty()){
+                int topEle = s1.top();
+                s2.push(topEle);
+                s1.pop();
+            }
+            int top = s2.top();
+            s2.pop();
+            return top;
         }
-        //ek case khud solve kardo
-        int temp =st.top();
-        st.pop();
-        pop();
-        st.push(temp);
-        return popedEle;
     }
     
     int peek() {
-        if(frontEle == -1){
-          return findFrontEle();
-        }
-        else{
-        return frontEle;
-        }
+       if(!s2.empty()){
+        return s2.top();
+       }
+       else{
+       while(!s1.empty()){
+                int topEle = s1.top();
+                s2.push(topEle);
+                s1.pop();
+            }
+            int top = s2.top();
+            return top;
+       }
     }
     
     bool empty() {
-        return st.empty();
+        return (s1.size() + s2.size() == 0) ? true : false; 
     }
 };
 
