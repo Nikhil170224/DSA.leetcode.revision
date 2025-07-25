0fc46cf88f3f403b733f4d0bf25368a9cb5bac85
Nikhil <nikhil150itc23@bpitindia.edu.in>
2025-07-25 13:03:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (35.97%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,27 +1,52 @@
+// class Solution {
+// public:
+//     int longestValidParentheses(string s) {
+//         stack<int>st;
+//         st.push(-1);
+//         int ans = INT_MIN;
+//         for(int i = 0; i < s.size(); i++){
+//             if(s[i] == '('){
+//                 st.push(i);
+//             }
+//             else{
+//                 st.pop();
+//                 if(!st.empty()){
+//                    int len = i- st.top(); 
+//                    ans = max(ans,len);
+//                 }
+//                 else{
+//                     st.push(i);
+//                 }
+//             }
+//         }
+//         if(ans <= 0){
+//             return 0;
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int longestValidParentheses(string s) {
         stack<int>st;
         st.push(-1);
-        int ans = INT_MIN;
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == '('){
-                st.push(i);
-            }
-            else{
+        int maxlen = 0;
+        for(int i = 0; i< s.size(); i++){
+            char currEle = s[i];
+            if(currEle == ')'){
                 st.pop();
-                if(!st.empty()){
-                   int len = i- st.top(); 
-                   ans = max(ans,len);
-                }
-                else{
+                if(st.empty()){
                     st.push(i);
                 }
+                else{
+                    int len = i - st.top();
+                    maxlen = max(maxlen, len);
+                }
+            }
+            else{
+                st.push(i);
             }
         }
-        if(ans <= 0){
-            return 0;
-        }
-        return ans;
+        return maxlen;
     }
 };
