193a9c59fa7e76dcdca72074569ea9b6bf1d2d37
Nikhil <nikhil150itc23@bpitindia.edu.in>
2025-07-26 00:14:17 +0530

    Time: 1 ms (56.47%) | Memory: 12.7 MB (74.42%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,24 +1,52 @@
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//         vector<int>ans;
+//         for(int i = 0; i< nums1.size(); i++){
+//             for(int j = 0; j < nums2.size(); j++){
+//                 if(nums1[i] == nums2[j]){
+//                     int k = j;
+//                     while(k < nums2.size()){
+//                         if(nums2[k] > nums2[j]){
+//                             ans.push_back(nums2[k]);
+//                             break;
+//                         }
+//                         else{
+//                             k++;
+//                         }
+//                     }
+//                     if(k == nums2.size())
+//                     ans.push_back(-1);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+//Optimisation-1 : Prestoring the index j for every value of nums1[i] in an Unordered_map
+
 class Solution {
 public:
+    unordered_map<int,int>indexOf;
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         vector<int>ans;
+        for(int j =0; j< nums2.size(); j++){
+            indexOf[nums2[j]] = j;
+        }
         for(int i = 0; i< nums1.size(); i++){
-            for(int j = 0; j < nums2.size(); j++){
-                if(nums1[i] == nums2[j]){
-                    int k = j;
-                    while(k < nums2.size()){
-                        if(nums2[k] > nums2[j]){
-                            ans.push_back(nums2[k]);
-                            break;
-                        }
-                        else{
-                            k++;
-                        }
+                int k = indexOf[nums1[i]];
+                while(k < nums2.size()){
+                    if(nums2[k] > nums1[i]){
+                        ans.push_back(nums2[k]);
+                        break;
                     }
-                    if(k == nums2.size())
-                    ans.push_back(-1);
+                    else{
+                        k++;
+                    }
                 }
-            }
+                if(k == nums2.size())
+                ans.push_back(-1);
         }
         return ans;
     }
