a32690a31e7aec3e09ad502067a4c6cc3670b423
Nikhil <nikhil150itc23@bpitindia.edu.in>
2025-07-28 19:43:48 +0530

    Time: 0 ms (100.00%) | Memory: 26.7 MB (46.05%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -30,39 +30,67 @@
 //     }
 // };
 
-// #2 Approach
+// // #2 Approach T.C = O(n2) || S.C = O(1)
+// class Solution {
+// public:
+//     void findRightMaxHeight(vector<int>& height, int& maxRightHeight, int& maxRightIdx){
+//         int i = maxRightIdx+1;
+//         for(i ; i< height.size(); i++){
+//             if(height[i] >= maxRightHeight){
+//                 maxRightHeight = height[i];
+//                 maxRightIdx = i;
+//             }
+//         }
+//         return;
+//     }
+//     int trap(vector<int>& height) {
+//         int totalWaterTrapped = 0;
+//         int maxRightHeight = 0, maxRightIdx = 0, maxLeftHeight = 0;
+//         findRightMaxHeight(height,maxRightHeight,maxRightIdx);
+//         for(int i = 0; i< height.size(); i++){
+//             // At every index i 
+//             // step 1 : find the left max heighted wall
+//             // step 2 : find the right max heighted wall
+//             // step 3 : sum = min(leftMaxheight,rightMaxheight) - currHeight;
+//             int currHeight = height[i];
+//             int waterTrapped = min(maxLeftHeight, maxRightHeight) - currHeight;
+//             if(waterTrapped > 0){
+//                 totalWaterTrapped += waterTrapped;
+//             }
+//             maxLeftHeight = max(maxLeftHeight,height[i]);
+//             // after i crosses the rightMaxHeight index we have to again call the function
+//             if(i == maxRightIdx-1){
+//                 maxRightHeight = 0;
+//                 findRightMaxHeight(height,maxRightHeight,maxRightIdx);
+//             }
+//         }
+//         return totalWaterTrapped;
+//     }
+// };
+
+// #3 Approach - we gona use an array to store the maxRightHeight for all the indices and after that What we gona do, we can ensure faster Lookups for maxRightHeight --> T.C = O(n) || S.C = O(n)
 class Solution {
 public:
-    void findRightMaxHeight(vector<int>& height, int& maxRightHeight, int& maxRightIdx){
-        int i = maxRightIdx+1;
-        for(i ; i< height.size(); i++){
-            if(height[i] >= maxRightHeight){
-                maxRightHeight = height[i];
-                maxRightIdx = i;
-            }
-        }
-        return;
+ void findRightMaxHeight(vector<int>& height, vector<int>& maxRightHeight){
+    int n = height.size();
+    int maxHeight = 0;
+    for(int i = n - 1; i >= 0; i--){
+        maxRightHeight[i] = maxHeight;
+        maxHeight = max(maxHeight, height[i]);
     }
+}
     int trap(vector<int>& height) {
+        vector<int>maxRightHeight(height.size(),0);
+        findRightMaxHeight(height,maxRightHeight);
         int totalWaterTrapped = 0;
-        int maxRightHeight = 0, maxRightIdx = 0, maxLeftHeight = 0;
-        findRightMaxHeight(height,maxRightHeight,maxRightIdx);
+        int maxLeftHeight = 0;
         for(int i = 0; i< height.size(); i++){
-            // At every index i 
-            // step 1 : find the left max heighted wall
-            // step 2 : find the right max heighted wall
-            // step 3 : sum = min(leftMaxheight,rightMaxheight) - currHeight;
             int currHeight = height[i];
-            int waterTrapped = min(maxLeftHeight, maxRightHeight) - currHeight;
+            int waterTrapped = min(maxLeftHeight, maxRightHeight[i]) - currHeight;
             if(waterTrapped > 0){
                 totalWaterTrapped += waterTrapped;
             }
             maxLeftHeight = max(maxLeftHeight,height[i]);
-            // after i crosses the rightMaxHeight index we have to again call the function
-            if(i == maxRightIdx-1){
-                maxRightHeight = 0;
-                findRightMaxHeight(height,maxRightHeight,maxRightIdx);
-            }
         }
         return totalWaterTrapped;
     }
