f835749ff521887eb6c35f7f12239f04575badf6
Nikhil <nikhil150itc23@bpitindia.edu.in>
2025-08-22 03:01:59 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (40.37%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -9,20 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     void preOrderTrav(TreeNode* root,vector<int>&preorder){
+//         if(root == NULL){
+//             return;
+//         }
+//         //ek case khud solve kardo
+//         preorder.push_back(root->val);
+//         preOrderTrav(root->left,preorder);
+//         preOrderTrav(root->right,preorder);
+//     }
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int>preorder;
+//         preOrderTrav(root,preorder);
+//         return preorder;
+//     }
+// };
+
 class Solution {
 public:
-    void preOrderTrav(TreeNode* root,vector<int>&preorder){
-        if(root == NULL){
-            return;
+  vector<int> preorderTraversal(TreeNode* root){
+        stack<TreeNode*>st;
+        vector<int>ans;
+        if(root==NULL) return ans;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* currNode = st.top();
+            st.pop();
+            ans.push_back(currNode->val);
+            if(currNode->right) st.push(currNode->right);
+            if(currNode->left) st.push(currNode->left);
         }
-        //ek case khud solve kardo
-        preorder.push_back(root->val);
-        preOrderTrav(root->left,preorder);
-        preOrderTrav(root->right,preorder);
-    }
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>preorder;
-        preOrderTrav(root,preorder);
-        return preorder;
+        return ans;
     }
 };
