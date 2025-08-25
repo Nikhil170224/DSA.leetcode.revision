62889f0d8cf39bb793e52b6852567124afb2a792
Nikhil <nikhil150itc23@bpitindia.edu.in>
2025-08-26 00:01:45 +0530

    Time: 1 ms (7.43%) | Memory: 11.1 MB (42.46%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -9,20 +9,45 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//         void postOrderTraversal(TreeNode* root,vector<int>&postorder){
+//         if(root == NULL){
+//             return;
+//         }
+//         //ek case khud solve kardo
+//         postOrderTraversal(root->left,postorder);
+//         postOrderTraversal(root->right,postorder);
+//         postorder.push_back(root->val);
+//         }
+//         vector<int> postorderTraversal(TreeNode* root) {
+//             vector<int>postorder;
+//             postOrderTraversal(root,postorder);
+//             return postorder;
+//         }
+// };
+
+// ITERATIVE SOLUTION :
 class Solution {
 public:
-        void postOrderTraversal(TreeNode* root,vector<int>&postorder){
-        if(root == NULL){
-            return;
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>res;
+        if(!root) return res;
+        stack<TreeNode*>st1;
+        stack<TreeNode*>st2;
+        st1.push(root);
+        while(!st1.empty()){
+            TreeNode* curr = st1.top();
+            st1.pop();
+            st2.push(curr);
+            if(curr->left) st1.push(curr->left);
+            if(curr->right) st1.push(curr->right);
         }
-        //ek case khud solve kardo
-        postOrderTraversal(root->left,postorder);
-        postOrderTraversal(root->right,postorder);
-        postorder.push_back(root->val);
+        while(!st2.empty()){
+            TreeNode* curr = st2.top();
+            res.push_back(curr->val);
+            st2.pop();
         }
-        vector<int> postorderTraversal(TreeNode* root) {
-            vector<int>postorder;
-            postOrderTraversal(root,postorder);
-            return postorder;
-        }
+        return res;
+    }
 };
