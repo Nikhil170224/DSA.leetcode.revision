b8f63441b60f1d92603abc8fe063aa701728f6d5
Nikhil <nikhil150itc23@bpitindia.edu.in>
2025-08-27 01:37:16 +0530

    Time: 3 ms (27.78%) | Memory: 17.1 MB (71.67%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -9,28 +9,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//         vector<vector<int>>ans;
+//         if(root == NULL) return ans;
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         while(!q.empty()){
+//         int levelSize = q.size();
+//         vector<int>level;
+//         for(int i = 0; i< levelSize; i++){
+//             TreeNode* temp = q.front();
+//             q.pop();
+//             level.push_back(temp->val);
+//             if(temp->left) q.push(temp->left);
+//             if(temp->right) q.push(temp->right);
+//         }
+//         ans.push_back(level);
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-      vector<vector<int>>result;
-      if(root == NULL) return result;
-
-      queue<TreeNode*>q;
-      q.push(root);
-      while(!q.empty()){
-        int levelSize = q.size();
-        vector<int>currentLevel;
-        for(int i = 0; i< levelSize; i++){
-            TreeNode* currentNode = q.front();
-            q.pop();
-            currentLevel.push_back(currentNode->val);
-
-            if(currentNode->left) q.push(currentNode->left);
-            if(currentNode->right) q.push(currentNode->right);
+        vector<vector<int>>res;
+        if(!root) return res;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            vector<int>ans;
+            for(int i = 0; i < size; i++){
+                TreeNode* curr = q.front();
+                q.pop();
+                ans.push_back(curr->val);
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+            res.push_back(ans);
         }
-        result.push_back(currentLevel);
-      }
-      return result;
-
+        return res;
     }
 };
