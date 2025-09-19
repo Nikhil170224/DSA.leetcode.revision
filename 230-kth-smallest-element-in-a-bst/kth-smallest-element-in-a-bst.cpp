c2cbc6367dcbcd33034b8a263ccb062def4177e4
Nikhil <nikhil150itc23@bpitindia.edu.in>
2025-09-19 20:21:17 +0530

    Time: 0 ms (100.00%) | Memory: 24.2 MB (99.22%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -11,18 +11,23 @@
  */
 class Solution {
 public:
-    void findkSmallest(vector<int>&ans,TreeNode* root,int k){
-        if(k == 0 || root == NULL) return;
+    void findkSmallest(int& ans,TreeNode* root,int& k){
+        if(root == NULL) return;
 
         findkSmallest(ans,root->left,k);
-        ans.push_back(root->val);
-        k--;
-        findkSmallest(ans,root->right,k);
-        
+        if(k > 0){
+            k--;
+            if(k == 0){
+            ans = root->val;
+            return;
+          }
+        }
+        findkSmallest(ans,root->right,k);        
     }
     int kthSmallest(TreeNode* root, int k) {
-        vector<int>ans;
+        // vector<int>ans;
+        int ans = -1;
         findkSmallest(ans,root,k);
-        return ans[k-1];
+        return ans;
     }
 };
