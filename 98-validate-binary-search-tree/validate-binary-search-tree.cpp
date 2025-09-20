e1c9d630e010b230276b734e54e97f4aaa6ccf99
Nikhil <nikhil150itc23@bpitindia.edu.in>
2025-09-20 18:49:40 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (75.21%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -11,32 +11,33 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int>&ans){
-        if(root==NULL){return;}
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
+    bool checkBST(TreeNode* root,long long minval,long long maxval){
+        if(root == NULL) return true;
+        if(root->val <= minval || root->val >= maxval) return false;
+
+        //if left is invalid return false without going to right subtree
+        if(!checkBST(root->left,minval,root->val))
+        return false;
+
+        if(!checkBST(root->right,root->val,maxval))
+        return false;
+
+        return true;
+
+        // //recursive calls
+        // // left RC
+        // // maxval = min(maxval,root->val);
+        // bool left = checkBST(root->left,minval,min(maxval,root->val));
+
+        // //right RC
+        // // minval = max(minval,root->val);
+        // bool right = checkBST(root->right,max(minval,root->val),maxval);
+
+        // return left && right;
+        
     }
     bool isValidBST(TreeNode* root) {
-    vector<int>ans;
-    inorder(root,ans);
-    int k=ans.size();
-    for(int i=1;i<k;i++){
-        if(ans[i]<=ans[i-1]){
-            return false;
-        }
-    }
-    return true;
+        return checkBST(root,LLONG_MIN,LLONG_MAX);
     }
 };
 
-    // //Base case
-    // if(root == NULL) return true;
-    // // if(root->left != NULL && root->left->val >= root->val) return false;
-    // // if(root->right != NULL && root->right->val <= root->val) return false;
-    //   // ek case khud solve kardo
-    //   bool ans1 = isValidBST(root->left);
-    
-    //   bool ans2 = isValidBST(root->right);
-
-    //   return (ans1 && ans2);
