657f4fa0ceb4dbaa729f1338e843e92d8cfb1bf5
Nikhil <nikhil150itc23@bpitindia.edu.in>
2025-09-21 23:31:50 +0530

    Time: 24 ms (45.28%) | Memory: 23.3 MB (90.21%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -7,32 +7,46 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+       if(root==NULL) return NULL;
+       int rootVal = root->val;
+       if(p->val > rootVal && q->val > rootVal)
+       return lowestCommonAncestor(root->right,p,q);
+       if(p->val < rootVal && q->val < rootVal)
+       return lowestCommonAncestor(root->left,p,q);
+       return root; 
+    }
+};
+
 // there are three cases possible :
 // 1. p,q both are greater than root
 // 2. p,q both are less than root
 // 3. p,q are inorder with root
 // we will traverse the BST and if we get a root inorder then we gona store the root as lca, else we move to right or left subtree
-class Solution {
-public:
-    void findLCA(TreeNode* root, TreeNode* p, TreeNode* q,TreeNode*& lca){
-        // Now here p is less than q
-        if(root==NULL) return;
-        if(p->val <= root->val && q->val >= root->val){
-            lca = root;
-            return;
-        }
-        else{
-            if(q->val < root->val && p->val < root->val) 
-            root = root->left;
-            else root = root->right;
-            findLCA(root,p,q,lca);
-        }
-    }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        TreeNode* lca = NULL;
-        TreeNode* a = (p->val < q->val) ? p : q;
-        TreeNode* b = (q->val > p->val) ? q : p;
-        findLCA(root,a,b,lca);
-        return lca;
-    }
-};
+// class Solution {
+// public:
+//     void findLCA(TreeNode* root, TreeNode* p, TreeNode* q,TreeNode*& lca){
+//         // Now here p is less than q
+//         if(root==NULL) return;
+//         if(p->val <= root->val && q->val >= root->val){
+//             lca = root;
+//             return;
+//         }
+//         else{
+//             if(q->val < root->val && p->val < root->val) 
+//             root = root->left;
+//             else root = root->right;
+//             findLCA(root,p,q,lca);
+//         }
+//     }
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         TreeNode* lca = NULL;
+//         TreeNode* a = (p->val < q->val) ? p : q;
+//         TreeNode* b = (q->val > p->val) ? q : p;
+//         findLCA(root,a,b,lca);
+//         return lca;
+//     }
+// };
